818deaed89afc6c46b0e1c9140df47b31cecd8f6
Pavel S <sadnessisasin@gmail.com>
2024-07-20 22:29:56 +0300

    users class added

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,30 +3,36 @@ import {OneUser} from "./OneUser/OneUser";
 import styles from "./Users.module.css";
 import axios from "axios";
 
-export const Users = (props) => {
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            if (props.users.length === 0) {
-                props.setUsers(response.data.items)
-            }
-        });
+export class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-    let usersElement = props.users.map(
-        m => <OneUser
-            follow={props.follow}
-            unfollow={props.unfollow}
-            id={m.id}
-            name={m.name}
-            status={m.status}
-            photo={m.photos.small}
-            location="location"
-            followed={m.followed}/>
-    );
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
 
-    return (
-        <div className={styles.container}>
-            {usersElement}
-        </div>
-    )
+    }
+
+    render() {
+
+        let usersElement = this.props.users.map(
+            m => <OneUser
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                id={m.id}
+                name={m.name}
+                status={m.status}
+                photo={m.photos.small}
+                location="location"
+                followed={m.followed}/>
+        );
+
+        return (
+            <div className={styles.container}>
+                {usersElement}
+            </div>
+        )
+    }
 }
