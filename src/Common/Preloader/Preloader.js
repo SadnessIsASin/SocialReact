afd9dd1522b28c539e41bc8c26f99e088dd76a3a
sadnessisasin <sadnessisasin@gmail.com>
2024-09-14 23:03:48 +0300

    test commit after 12 september

--- src/Common/Preloader/Preloader.js
+++ src/Common/Preloader/Preloader.js
@@ -2,7 +2,7 @@ import styles from "../../components/Users/Users.module.css";
 import preloader from "../../assets/preloader.svg";
 import React from "react";
 
-export const Preloader = (props) => {
+export const Preloader = () => {
     return <div className={styles.preloader}>
         <img src={preloader} alt=""/>
     </div>
